ed16a7de5742a3a75737c615e1ea97a503714f47
Samir Hassan <hsamir347@gmail.com>
2024-09-29 13:34:33 -0400

    created the route for querying db and got function from controller

--- server/routes/produce.js
+++ server/routes/produce.js
@@ -1,19 +1,17 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import produceData from "../data/produce.js";
+import produceController from "../controllers/produce.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json(produceData);
-});
+router.get("/", produceController.getGifts);
 
 router.get("/:produceId", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "../public/produce.html"));
 });
 
-export default router
+export default router;
